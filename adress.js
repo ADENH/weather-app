41c07aa02af07f3a97b8823a0d386692bfbd58ef
Ade Nur Hidayat <adenurhidayat101294@gmail.com>
2018-10-31 15:27:51 +0700

    error handling
    error handling for request

--- adress.js
+++ adress.js
@@ -6,17 +6,22 @@ var getInfo= (adress)=>{
         url:`http://www.mapquestapi.com/geocoding/v1/address?key=qYgFlK2HIXSbxyDGexwTfCuz8XHgnemX&location=${encodeURIComponent(adress)}`,
         json:true
     },(error,response,body)=>{
-        if(body.results[0].locations[0]!=null){
-            console.log(`Ini nama Jalannya : ${body.results[0].locations[0].street}`);
-            console.log(`Ini nama daerahnya : ${body.results[0].locations[0].adminArea5}`);
-            console.log(`ini kordinat latitude : ${body.results[0].locations[0].latLng.lat}`);
-            console.log(`Ini kordinat longitude : ${body.results[0].locations[0].latLng.lng}`); 
-        // format object readable
-        console.log(JSON.stringify(response,undefined,2));
+        if(error){
+            console.log('Unable to connect');
         }else{
-            console.log('Location Not Found');
-            console.log(JSON.stringify(response,undefined,2));
+            if(body.results[0].locations[0]!=null){
+                console.log(`Ini nama Jalannya : ${body.results[0].locations[0].street}`);
+                console.log(`Ini nama daerahnya : ${body.results[0].locations[0].adminArea5}`);
+                console.log(`ini kordinat latitude : ${body.results[0].locations[0].latLng.lat}`);
+                console.log(`Ini kordinat longitude : ${body.results[0].locations[0].latLng.lng}`); 
+            // format object readable
+            //console.log(JSON.stringify(response,undefined,2));
+            }else{
+                console.log('Location Not Found');
+                console.log(JSON.stringify(response,undefined,2));
+            }
         }
+        
             });
 
 
